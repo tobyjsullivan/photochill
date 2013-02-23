36ba85ce89e3c3451710ca9b45f9b0e5e246d23c
Toby Sullivan <tobyjsullivan@gmail.com>
2013-02-23 13:58:50 -0800

    Cleaned up composer implementation. Removed deployed vendor stuff

--- upload-server/src/htdocs/index.php
+++ upload-server/src/htdocs/index.php
@@ -6,9 +6,12 @@ function __autoload($className) {
 	
 	include_once('../library/'.$className.'.php');
 }
+
+require('../library/vendor/autoload.php');
+
 require_once('../config/configuration.php');
 
-use \Aws\AwsS3Adapter;
+// use \Aws\AwsS3Adapter;
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,25 +22,26 @@ use \Aws\AwsS3Adapter;
 	<h1>Under Construction</h1>
 	<p>We are still developing the upload server.</p>
 	<?php
+	echo Chill\Configure::read('AWS_ACCESS_KEY_ID');
 // 	$api = new AwsS3Adapter();
 // 	$api->getObjects();
 
-	$api = new S3();
-	$api->setAuth(Configure::read('AWS_ACCESS_KEY_ID'), Configure::read('AWS_SECRET_ACCESS_KEY'));
-// 	$api->setEndpoint(Configure::read('AWS_S3_BUCKET_HOSTNAME'));
+// 	$api = new S3();
+// 	$api->setAuth(Chill\Configure::read('AWS_ACCESS_KEY_ID'), Chill\Configure::read('AWS_SECRET_ACCESS_KEY'));
+// 	$api->setEndpoint(Chill\Configure::read('AWS_S3_BUCKET_HOSTNAME'));
 	
-	$buckets = $api->listBuckets(true);
+// 	$buckets = $api->listBuckets(true);
 	
-	var_dump($buckets);
+// 	var_dump($buckets);
 	?>
 	<br />
 	<?php 
-	$bucket_api = new S3('http://s3-ap-southeast-1.amazonaws.com');
-	$bucket_api->setAuth(Configure::read('AWS_ACCESS_KEY_ID'), Configure::read('AWS_SECRET_ACCESS_KEY'));
-	//$bucket_api->setEndpoint(Configure::read('AWS_S3_BUCKET_HOSTNAME'));
-	$objects = $bucket_api->getBucket('rawchill');
+// 	$bucket_api = new S3('http://s3-ap-southeast-1.amazonaws.com');
+// 	$bucket_api->setAuth(Chill\Configure::read('AWS_ACCESS_KEY_ID'), Chill\Configure::read('AWS_SECRET_ACCESS_KEY'));
+// 	$bucket_api->setEndpoint(Chill\Configure::read('AWS_S3_BUCKET_HOSTNAME'));
+// 	$objects = $bucket_api->getBucket('rawchill');
 	
-	var_dump($objects);
+// 	var_dump($objects);
 	?>
 </body>
 </html>
